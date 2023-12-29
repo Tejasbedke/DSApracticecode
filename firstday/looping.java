40bc005911a7d98c992cb9dc6fcf54dcb7050007
Tejas <tejasbedke@gmail.com>
2023-12-29 08:26:56 +0530

    first day

---
+++ firstday/looping.java
@@ -0,0 +1,20 @@
+package firstday;
+class looping{
+
+    public static void main(String []args){
+
+        //for loop 
+        for(int i=0;i<10;i++){
+
+            System.out.println("Iteration :-"+i);
+        }
+
+        //while loop
+        int j=0;
+        while(j<10){
+
+            System.out.println("Iteration :-"+j);
+            j++;
+        }
+    }
+}
